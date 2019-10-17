7457e71539ec445795f92b28caadb4a40055d3f9
Golnaz Ghodoosipour <golghp@Golnazs-Air.attlocal.net>
2019-10-16 23:25:55 -0700

    modified quick sort

--- Sort/QuickSort.cpp
+++ Sort/QuickSort.cpp
@@ -3,11 +3,6 @@
 
 void partition(int left, int high, int pivot, std::vector<int>& arr)
 {
-    // std::cout<<"I've entered partition"<<std::endl;
-    // for (int i = 0; i < arr.size(); i++)
-    // {
-    //     std::cout<<"partition array"<<arr[i]<<std::endl;
-    // }
     int count = 0;
     int tmp = 0;
     while(left < high)
@@ -31,38 +26,16 @@ void partition(int left, int high, int pivot, std::vector<int>& arr)
         arr[count] = arr[high];
         arr[high] = tmp;
     }
-
-    // std::cout<<"here we go"<<std::endl;
-    // for (int i = 0; i < arr.size(); i++)
-    // {
-    //     std::cout<<"partition array"<<arr[i]<<std::endl;
-    // }
 }
 
 void QuickSort(std::vector<int>& arr, int low, int high)
 {
-    // std::cout<<"I've entered QuickSort"<<std::endl;
-    // for (int i = 0; i < arr.size(); i++)
-    // {
-    //     std::cout<<"partition array"<<arr[i]<<std::endl;
-    // }
-
     int pivot = high;
     partition(low, high, high, arr);
 
     QuickSort(arr, low, pivot - 1);
 
-    // for (int i = 0; i < arr.size(); i++)
-    // {
-    //     std::cout<<"first array"<<arr[i]<<std::endl;
-    // }
-
     QuickSort(arr, pivot + 1, high);
-
-    // for (int i = 0; i < arr.size(); i++)
-    // {
-    //     std::cout<<"second array"<<arr[i]<<std::endl;
-    // }
 }
 
 int main() {
